a73d109ecfdacc429302bf8965a5e48bc7f51d80
Max <maxime.hadj@laplateforme.io>
2023-10-03 10:32:11 +0200

    added some talents description

--- src/talents.js
+++ src/talents.js
@@ -1,9 +1,10 @@
-
 export const talents = [
     {
         id: 1,
         name: 'Improved Eviscerate',
-        desc: 'Increases the damage done by your Eviscerate ability by 5%.',
+        desc: 'Increases the damage done by your Eviscerate ability by {value}.',
+        value: [5, 10, 15],
+        nullvalue: null,
     },
     {
         id: 2,
@@ -13,7 +14,7 @@ export const talents = [
     {
         id: 3,
         name: 'Malice',
-        desc: 'Increases your critical strike chance by 1%',
+        desc: 'Increases your critical strike chance by 1%.',
     },
     {
         id: 4,
@@ -30,4 +31,125 @@ export const talents = [
         name: 'Improved Slice and Dice',
         desc: 'Increases the duration of your Slice and Dice ability by 15%.',
     },
+    {
+        id: 7,
+        name: 'Relentless Strikes',
+        desc: 'Your finishing moves have a 20% chance per combo point to restore 25 energy.'
+    },
+    {
+        id: 8,
+        name:'Improved Expose Armor',
+        desc:'Increases the armor reduced by your Expose Armor ability by 25%.',
+    },
+    {
+        id: 9,
+        name:'Lethality',
+        desc:'Increases the critical strike damage bonus of your Sinister Strike, Gouge, Backstab, Ghostly Strike, and Hemorrhhage abilities by 6%.',
+    },
+    {
+        id: 10,
+        name:'Vile Poisons',
+        desc:'Increases the damage dealt by your poisons by 4% and gives your poisons an additional 8% chance to resist dispel effects.',
+    },
+    {
+        id: 11,
+        name:'Improved Poisons',
+        desc:'Increases the chance to apply poisons to your target by 2%.',
+    },
+    {
+        id: 12,
+        name:'Cold Blood',
+        desc:'When activated, increases the critical strike chance of your next Sinister Strike, Backstab, Ambush, or Eviscerate by 100%.',
+    },
+    {
+        id: 13,
+        name:'Improved Kidney Shot',
+        desc:'While affected by your Kidney Shot ability, the target receives an additional 3% damage from all sources.',
+    },
+    {
+        id: 14,
+        name:'Seal Fate',
+        desc:'Your critical strikes from abilities that add combo points have a 20% chance to add an additional combo point.',
+    },
+    {
+        id: 15,
+        name:'Vigor',
+        desc:'Increases your maximum Energy by 10.',
+    },
+    {
+        id: 16,
+        name:'',
+        desc:'',
+    },
+    {
+        id: 17,
+        name:'',
+        desc:'',
+    },
+    {
+        id: 18,
+        name:'',
+        desc:'',
+    },
+    {
+        id: 19,
+        name:'',
+        desc:'',
+    },
+    {
+        id: 20,
+        name:'',
+        desc:'',
+    },
+    {
+        id: 21,
+        name:'',
+        desc:'',
+    },
+    {
+        id: 22,
+        name:'',
+        desc:'',
+    },
+    {
+        id: 23,
+        name:'',
+        desc:'',
+    },
+    {
+        id: 24,
+        name:'',
+        desc:'',
+    },
+    {
+        id: 25,
+        name:'',
+        desc:'',
+    },
+    {
+        id: 26,
+        name:'',
+        desc:'',
+    },
+    {
+        id: 27,
+        name:'',
+        desc:'',
+    },
+    {
+        id: 28,
+        name:'',
+        desc:'',
+    },
+    {
+        id: 29,
+        name:'',
+        desc:'',
+    },
+    {
+        id: 30,
+        name:'',
+        desc:'',
+    },
+
 ]
